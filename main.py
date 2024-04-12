f52d355b610a62dd1ffda1aa553b4662651c5eae
CrazzyyyCoder <140911076+CrazzyyyCoder@users.noreply.github.com>
2024-04-12 19:03:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ tree = discord.app_commands.CommandTree(client)
 @client.event
 async def on_ready():
     await tree.sync()
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Pogi Bot'), status=discord.Status.dnd)
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Your Gay'), status=discord.Status.dnd)
     print('Logged in')
     print('------')
     print(client.user.display_name)
