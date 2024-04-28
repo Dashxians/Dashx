408bc20dbf5d0b19cf71388d2f33d102264ca8c3
CrazzyyyCoder <140911076+CrazzyyyCoder@users.noreply.github.com>
2024-04-29 00:02:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,10 @@ rbxlx_files = {
     "tt": {
         "theme_name": "Roblox Theme Paid",
         "file_location": "Files/Roblox Theme Paid.rbxlx"
+    }
+    "ps": {
+        "theme_name": "Pet Sim Theme Paid",
+        "file_location": "Files/Pet_Sim_Theme.rbxlx"
     },
     # Add more themes here as needed
 }
